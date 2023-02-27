5c2a1493cb81beef0e36522ac1bc96629ba88805
duy.nguyen_huu@napaglobal.com <duy.nguyen_huu>
2023-02-27 18:03:41 +0700

    [MOD] add dom event with useEffect

--- src/Content.js
+++ src/Content.js
@@ -5,6 +5,8 @@ function Content() {
   const [data, setData] = useState([]);
   const action = ["posts", "comments", "todos"];
   const [type, setType] = useState("posts");
+  const [showtop, setTop] = useState(false);
+
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${type}`).then((response) => {
       response.json().then((res) => {
@@ -12,6 +14,23 @@ function Content() {
       });
     });
   }, [type]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 100) {
+        setTop(true);
+      } else {
+        setTop(false);
+      }
+      // setTop(window.scrollY)
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScrolly = () => {
+    setTop(false);
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div style={{ margin: 30 }}>
@@ -30,6 +49,14 @@ function Content() {
         ))}
       </div>
       <input onChange={(e) => setTitle(e.target.value)} />
+      {showtop && (
+        <button
+          onClick={() => handleScrolly()}
+          style={{ position: "fixed", right: 20, bottom: 20 }}
+        >
+          top
+        </button>
+      )}
       <ul>
         {data.map((item) => {
           return <li key={item.id}>{item.title || item.email}</li>;
