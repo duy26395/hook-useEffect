49bea88114c0a7b50bd2e7b8c8b58d6effcab2ff
duy.nguyen_huu@napaglobal.com <duy.nguyen_huu>
2023-02-27 13:44:15 +0700

    [MOD] add func use effect with deps

--- src/Content.js
+++ src/Content.js
@@ -1,32 +1,41 @@
 import { useEffect, useState } from "react";
 
-
 function Content() {
-    const [title, setTitle] = useState('');
-    const [data, setData] = useState([]);
+  const [title, setTitle] = useState("");
+  const [data, setData] = useState([]);
+  const action = ["posts", "comments", "todos"];
+  const [type, setType] = useState("posts");
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`).then((response) => {
+      response.json().then((res) => {
+        setData(res);
+      });
+    });
+  }, [type]);
 
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                response.json().then(res => {
-                    setData(res)
-                })
-            })
-    }, [])
-
-    return (
-        <div style={{ margin: 30 }}>
-            <h1>
-                {title}
-            </h1>
-            <input onChange={e => setTitle(e.target.value)} />
-            <ul>
-                {data.map((item) => {
-                    return <li key={item.id}>{item.title}</li>
-                })}
-            </ul>
-        </div>
-    )
+  return (
+    <div style={{ margin: 30 }}>
+      <h1>{title}</h1>
+      <div>
+        {action.map((item, index) => (
+          <button
+            key={index}
+            onClick={() => setType(item)}
+            style={
+              type === item ? { color: "#0454", backgroundColor: "#fff" } : {}
+            }
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+      <input onChange={(e) => setTitle(e.target.value)} />
+      <ul>
+        {data.map((item) => {
+          return <li key={item.id}>{item.title || item.email}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 export default Content;
