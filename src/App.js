9a25be24fcb32eee7af9bdd5f49aafd513ba6eba
duydepzai <duy26395@gmail.com>
2023-03-14 23:21:34 +0700

    [MOD] useref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import Content from "./Content"
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 function App() {
-  const [show, setShow] = useState(false)
+  const [count, setCount] = useState(60)
+  const timer = useRef()
+  const old = useRef()
+  const refEl = useRef()
+
+  useEffect(() => {
+    old.current = count
+  }, [count])
+
+  useEffect(() => {
+    console.log(refEl.current);
+  }, [count])
+
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setCount(prevcount => prevcount - 1);
+    }, 1000);
+  }
+
+
+  const handleStop = () => {
+    clearInterval(timer.current)
+  }
+
+  console.log('old',old.current);
+  console.log('count',count);
+
   return (
     <div style={{margin : 30}}>
-      <button onClick={() => { setShow(!show) }}> toogle</button>
-      {show && <Content />}
+      <h1 ref={refEl}>{count}</h1>
+      <button onClick={handleStart}> Start</button>
+      <button onClick={handleStop}> Stop</button>
     </div>
   );
 }
